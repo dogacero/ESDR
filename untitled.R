8c3a0b9a4fd08b48ed3d0f9c049c65b7a089c1ad
dogacero <dogaceren2002@hotmail.com>
2024-11-23 14:18:13 +0100

    g

---
+++ untitled.R
@@ -0,0 +1,14 @@
+---
+  title: "Lösungen der Übungsaufgaben"
+author: "Doga Ceren Bozkurt-Sezin Özer"
+date: "2024-11-23"
+output: html_document
+---
+
+
+
+```{r}
+ITSDaten_Gruppe3_MTZ <- read.csv2(file = "ITSDaten_Gruppe3_MTZ.csv", fileEncoding = "utf-8", stringsAsFactors = TRUE)
+
+```
+
